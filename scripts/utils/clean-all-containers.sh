0bbc374c23d1dc41cfe556793f21ea30c885a7f8
Fabrizio Torelli <hellgate75@gmail.com>
2020-02-15 23:07:44 +0000

    definition of some new procedures

--- scripts/utils/clean-all-containers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-LIST="$(docker ps -a|grep -v IMAGE | awk 'BEGIN {FS=OFS=" "}{print $1}'|xargs echo)"
-IFS=" ";for id in $LIST; do
-      docker stop $id
-      if [[ "[]" != "$(docker inspect $id 2>/dev/null)" ]]; then
-      	docker rm $id
-      fi
-done
